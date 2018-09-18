010fa1a7bcda4f968bb126eb58df267c5e665d83
RandolphCarter <bm121990@gmail.com>
2018-09-18 17:44:16 +0300

    Clean up project structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,43 +2,46 @@ const gulp = require('gulp');
 const watch = require('gulp-watch');
 const less = require('gulp-less');
 const browserSync = require('browser-sync').create();
-const htmltidy = require('gulp-htmltidy');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
+const clean = require('gulp-clean');
+const runSequence = require('run-sequence');
+const changed = require('gulp-changed');
 
 gulp.task('less', function () {
-    return gulp.src('./src/less/**/*.less')
+    return gulp.src('./src/less/styles.less')
         .pipe(less())
         .on('error', function (err) {
             console.log(err.toString());
             this.emit('end');
         })
-        .pipe(gulp.dest('./src/css'))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest('build/css'))
         .pipe(browserSync.reload({
                 stream: true
             }))
 });
 
 gulp.task('watch',['browserSync'], function () {
-    gulp.watch('./src/less/**/*.less',['less']);
-    gulp.watch('./src/**/*.html',['less']);
-})
+    gulp.watch('src/less/**/*.less',['less']);
+    gulp.watch('src/*.html',['copyHtml',browserSync.reload]);
+    gulp.watch('src/js/*.js',['copyJs',browserSync.reload]);
+});
 
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
-            baseDir: './src/'
+            baseDir: './build/'
         },
     })
-})
-
-gulp.task('htmltidy', function() {
-    return gulp.src('./src/*.html')
-        .pipe(htmltidy())
-        .pipe(gulp.dest('./build/'));;
 });
 
-gulp.task('build',function () {
+gulp.task('buildStyles',function () {
     return gulp.src('./src/less/**/*.less')
         .pipe(less())
         .pipe(autoprefixer({
@@ -48,5 +51,37 @@ gulp.task('build',function () {
         .pipe(gulp.dest('./src/css'))
         .pipe(cssnano())
         .pipe(gulp.dest('./build/css'))
+});
+
+gulp.task('images', function () {
+    let dest = 'build/img';
+    return gulp.src('src/img/**')
+        .pipe(changed(dest))
+        .pipe(imagemin())
+        .pipe(gulp.dest(dest))
+});
+
+gulp.task('build',function () {
+    runSequence('removeBuildFolder',['buildStyles','copyJs','images','copyHtml']);
+    
+});
+
+gulp.task('copyHtml',function () {
+    let dest = 'src/*.html';
+    return gulp.src(dest)
+        .pipe(changed(dest))
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('removeBuildFolder', function () {
+    return gulp.src('build', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('copyJs',function () {
+    let dest = 'src/js/*.js';
+    return gulp.src(dest)
+        .pipe(changed(dest))
+        .pipe(gulp.dest('build/js'))
 });
 
